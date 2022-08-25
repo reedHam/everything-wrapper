79c0953329deb7a78e1b41e70dab904c73a78034
Reed <reed.hambrook@gmail.com>
2022-08-25 10:05:33 -0700

    refactored modules

--- src/lib.rs
+++ src/lib.rs
@@ -2,49 +2,55 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-
-use widestring::*;
-
-pub fn version() -> String {
-    unsafe {
-        let major = Everything_GetMajorVersion();
-        let minor = Everything_GetMinorVersion();
-        let revision = Everything_GetRevision();
-        format!("{}.{}.{}", major, minor, revision)
-    }
+mod EverythingSDK {
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
 
-pub fn query_sync(query: &str) -> DWORD {
-    let query_c_str = match U16CString::from_str(query) {
-        Ok(res) => res,
-        Err(_) => return 0,
-    };
-    unsafe {
-        Everything_SetSearchW(query_c_str.as_ptr());
-        Everything_SetRequestFlags(EVERYTHING_REQUEST_FILE_NAME | EVERYTHING_REQUEST_PATH);
-        Everything_QueryW(true as BOOL);
-        Everything_GetNumResults()
+pub mod everything {
+    use super::EverythingSDK::*;
+    use widestring::*;
+
+    pub fn version() -> String {
+        unsafe {
+            let major = Everything_GetMajorVersion();
+            let minor = Everything_GetMinorVersion();
+            let revision = Everything_GetRevision();
+            format!("{}.{}.{}", major, minor, revision)
+        }
+    }
+
+    pub fn set_search(search: &str) {
+        let search_c_str = match U16CString::from_str(search) {
+            Ok(res) => res,
+            Err(_) => panic!("Could not convert query to U16CString"),
+        };
+
+        unsafe {
+            Everything_SetSearchW(search_c_str.as_ptr());
+        }
+    }
+
+    pub fn query(query: &str) -> DWORD {
+        unsafe {
+            Everything_QueryW(true as BOOL);
+            Everything_GetNumResults()
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use widestring::u16cstr;
-
-    use super::*;
+    use super::everything::*;
 
     #[test]
     fn reports_version() {
-        unsafe {
-            let version = version();
-            assert_eq!(version, "1.4.1");
-        }
+        let version = version();
+        assert_eq!(version, "1.4.1");
     }
 
     #[test]
-    fn gets_results() {
-        let num_results = query_sync("");
+    fn queries() {
+        let num_results = query("");
         println!("Everything num results: {}", num_results);
         assert!(num_results > 0);
     }
