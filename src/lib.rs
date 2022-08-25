7b33a1d3016ce4d21915e2690ded76cdfc9a1423
Reed <reed.hambrook@gmail.com>
2022-08-25 09:37:09 -0700

    added save version function

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,16 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 use widestring::*;
 
-pub fn query(query: &str) -> DWORD {
+pub fn version() -> String {
+    unsafe {
+        let major = Everything_GetMajorVersion();
+        let minor = Everything_GetMinorVersion();
+        let revision = Everything_GetRevision();
+        format!("{}.{}.{}", major, minor, revision)
+    }
+}
+
+pub fn query_sync(query: &str) -> DWORD {
     let query_c_str = match U16CString::from_str(query) {
         Ok(res) => res,
         Err(_) => return 0,
@@ -28,21 +37,14 @@ mod tests {
     #[test]
     fn reports_version() {
         unsafe {
-            let major = Everything_GetMajorVersion();
-            let minor = Everything_GetMinorVersion();
-            let revision = Everything_GetRevision();
-            println!("Everything major version: {}", major);
-            println!("Everything minor version: {}", minor);
-            println!("Everything revision: {}", revision);
-            assert_eq!(major, 1);
-            assert_eq!(minor, 4);
-            assert_eq!(revision, 1);
+            let version = version();
+            assert_eq!(version, "1.4.1");
         }
     }
 
     #[test]
     fn gets_results() {
-        let num_results = query("");
+        let num_results = query_sync("");
         println!("Everything num results: {}", num_results);
         assert!(num_results > 0);
     }
