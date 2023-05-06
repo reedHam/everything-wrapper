b58eaff40c6eeac778b8dfbd7b1b6bea72ba5f86
Reed <reed.hambrook@gmail.com>
2023-05-06 16:30:17 -0700

    updated node api

--- everything_sys/build.rs
+++ everything_sys/build.rs
@@ -1,12 +1,10 @@
-extern crate bindgen;
-
-use std::fs::canonicalize;
-use std::path::*;
-use std::{env, fs};
+use std::env;
+use std::fs;
+use std::path::{Path, PathBuf};
 
 fn main() {
     // Code modified from https://rust-lang.github.io/rust-bindgen/tutorial-3.html
-    let everything_sdk_path = canonicalize(Path::new(r#"..\Everything-SDK\lib"#)).unwrap();
+    let everything_sdk_path = fs::canonicalize(Path::new(r#"..\Everything-SDK\lib"#)).unwrap();
     println!("cargo:rustc-link-search={}", everything_sdk_path.display());
 
     println!("cargo:rustc-link-lib=Everything64");
@@ -30,8 +28,12 @@ fn main() {
         .expect("Couldn't write bindings!");
 
     let target_dir = env::var("CARGO_TARGET_DIR").unwrap_or_else(|_| "target".to_string());
-    let profile = env::var("PROFILE").expect("PROFILE environment variable not found");
-    let dll_path = Path::new("../Everything-SDK/dll/Everything64.dll");
+    let target_feature = env::var("CARGO_CFG_TARGET_FEATURE").unwrap_or_default();
+    let is_release = target_feature.contains("crt-static");
+
+    let profile = if is_release { "release" } else { "debug" };
+
+    let dll_path = Path::new(r"C:\github\everything-wrapper\Everything-SDK\dll\Everything64.dll");
 
     let target_path = Path::new(&target_dir)
         .join(profile)
@@ -39,6 +41,9 @@ fn main() {
         .join("Everything64.dll");
 
     if !target_path.exists() {
-        fs::copy(dll_path, target_path).expect("Failed to copy Everything.dll");
+        fs::copy(&dll_path, &target_path).expect(&format!(
+            "Failed to copy Everything.dll from {:?} to {:?}",
+            dll_path, target_path
+        ));
     }
 }
