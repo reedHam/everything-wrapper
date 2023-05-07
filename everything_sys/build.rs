c2599f6be51b4af0a9d270a25a62f5c7e5273501
Reed <reed.hambrook@gmail.com>
2023-05-06 21:45:16 -0700

    OUT_DIR not defined when publishing

--- everything_sys/build.rs
+++ everything_sys/build.rs
@@ -22,8 +22,7 @@ fn main() {
         .generate()
         .expect("Unable to generate bindings");
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
-        .write_to_file(out_path.join("bindings.rs"))
+        .write_to_file("bindings.rs")
         .expect("Couldn't write bindings!");
 }
