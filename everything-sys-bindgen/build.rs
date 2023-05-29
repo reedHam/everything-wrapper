fbfd033414156c64b1904348da3b35e5af1d5e1f
Reed <reed.hambrook@gmail.com>
2023-05-28 18:18:51 -0700

    added windows config flags

--- everything-sys-bindgen/build.rs
+++ everything-sys-bindgen/build.rs
@@ -1,25 +1,28 @@
 use std::env;
 
 fn main() {
-    println!(
-        "cargo:rustc-link-search={}",
-        env::current_dir().unwrap().display()
-    );
-    println!("cargo:rustc-link-lib=dylib=Everything64");
-    println!("cargo:rerun-if-changed=wrapper.h");
+    #[cfg(target_os = "windows")]
+    {
+        println!(
+            "cargo:rustc-link-search={}",
+            env::current_dir().unwrap().display()
+        );
+        println!("cargo:rustc-link-lib=dylib=Everything64");
+        println!("cargo:rerun-if-changed=wrapper.h");
 
-    let everything_regex_filter = "Everything.*|EVERYTHING.*";
+        let everything_regex_filter = "Everything.*|EVERYTHING.*";
 
-    let bindings = bindgen::Builder::default()
-        .allowlist_function(everything_regex_filter)
-        .allowlist_type(everything_regex_filter)
-        .allowlist_var(everything_regex_filter)
-        .header("Everything.h")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .generate()
-        .expect("Unable to generate bindings");
+        let bindings = bindgen::Builder::default()
+            .allowlist_function(everything_regex_filter)
+            .allowlist_type(everything_regex_filter)
+            .allowlist_var(everything_regex_filter)
+            .header("Everything.h")
+            .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+            .generate()
+            .expect("Unable to generate bindings");
 
-    bindings
-        .write_to_file("Everything.rs")
-        .expect("Couldn't write bindings!");
+        bindings
+            .write_to_file("Everything.rs")
+            .expect("Couldn't write bindings!");
+    }
 }
--- everything-sys-bindgen/src/lib.rs
+++ everything-sys-bindgen/src/lib.rs
@@ -2,6 +2,7 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+#[cfg(target_os = "windows")]
 include!("../Everything.rs");
 
 #[cfg(test)]
